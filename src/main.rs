ac40863c531d45aebc0ca60a8b4e2f808b969691
Marc Poulhiès <dkm@kataplop.net>
2025-09-16 22:33:31 +0200

    refactor: use an optional config file
    All options can be set in the config file.
    Command line options override values from the config file, if any.
    Makes the default values a bit hidden (previously handled by clap).

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use serde::Deserialize;
 use std::fs;
+use std::io::Error;
+
 use std::path::PathBuf;
 
 use chrono::Local;
@@ -13,6 +16,129 @@ pub mod git;
 mod parser;
 mod rebaseupstream;
 mod upstream;
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    work: Option<String>,
+    github_project_owner: Option<String>,
+    gcc_upstream_branch: Option<String>,
+    gccrs_dev_branch: Option<String>,
+    remote: Option<String>,
+    token_file: Option<String>,
+    to_branch: Option<String>,
+    no_fetch: Option<bool>,
+    linearize_only: Option<bool>,
+    autosquash: Option<bool>,
+    github_upstream_base: Option<String>,
+    no_rebase: Option<bool>,
+}
+
+impl Config {
+    fn new() -> Self {
+        Config {
+            work: None,
+            github_project_owner: None,
+            gcc_upstream_branch: None,
+            gccrs_dev_branch: None,
+            remote: None,
+            token_file: None,
+            to_branch: None,
+            no_fetch: None,
+            linearize_only: None,
+            autosquash: None,
+            github_upstream_base: None,
+            no_rebase: None,
+        }
+    }
+
+    fn to_upstreamopt(
+        &mut self,
+        token_file: Option<String>,
+        github_project_owner: Option<String>,
+        github_upstream_base: Option<String>,
+        no_fetch: u8,
+        no_rebase: u8,
+        new_branch: Option<String>,
+        gcc_upstream_branch: Option<String>,
+        gccrs_dev_branch: Option<String>,
+        gccrs: Option<PathBuf>,
+        remote: Option<String>,
+    ) -> Result<upstream::UpstreamOpt, Error> {
+        let token = if let Some(tf) = token_file {
+            Some(fs::read_to_string(&tf)?.trim().to_string())
+        } else if let Some(tf) = &self.token_file {
+            Some(fs::read_to_string(&tf)?.trim().to_string())
+        } else {
+            None
+        };
+        Ok(upstream::UpstreamOpt {
+            token,
+            github_project_owner: github_project_owner.or(self.github_project_owner.take()),
+            github_upstream_base: github_upstream_base.or(self.github_upstream_base.take()),
+            no_fetch: no_fetch > 0 || self.no_fetch.map_or(false, |v| v),
+            no_rebase: no_rebase > 0 || self.no_rebase.map_or(false, |v| v),
+            new_branch: new_branch
+                .or(self.to_branch.take())
+                .or(Some(create_new_branch_name("rebase")))
+                .unwrap(),
+            gcc_upstream_branch: gcc_upstream_branch
+                .or(self.gcc_upstream_branch.take())
+                .or(Some("gnu/trunk".to_string()))
+                .unwrap(),
+            gccrs_dev_branch: gccrs_dev_branch
+                .or(self.gccrs_dev_branch.take())
+                .or(Some("upstream-gccrs/master".to_string()))
+                .unwrap(),
+            gccrs: gccrs.or(self.work.take().map(PathBuf::from)).unwrap(),
+            remote: remote.or(self.remote.take()),
+        })
+    }
+
+    fn to_rebaseupstreamopt(
+        &mut self,
+        token_file: Option<String>,
+        github_project_owner: Option<String>,
+        autosquash: u8,
+        linearize_only: u8,
+        no_fetch: u8,
+        gcc_upstream_branch: Option<String>,
+        gccrs_dev_branch: Option<String>,
+        new_branch: Option<String>,
+        gccrs: Option<PathBuf>,
+        remote: Option<String>,
+    ) -> Result<rebaseupstream::RebaseUpstreamOpt, Error> {
+        let token = if let Some(tf) = token_file {
+            Some(fs::read_to_string(&tf)?.trim().to_string())
+        } else if let Some(tf) = &self.token_file {
+            Some(fs::read_to_string(&tf)?.trim().to_string())
+        } else {
+            None
+        };
+        println!("{:#?}", self);
+
+        Ok(rebaseupstream::RebaseUpstreamOpt {
+            token: token,
+            github_project_owner: github_project_owner.or(self.github_project_owner.take()),
+            autosquash: autosquash > 0 || self.autosquash.map_or(false, |v| v),
+            linearize_only: linearize_only > 0 || self.linearize_only.map_or(false, |v| v),
+            no_fetch: no_fetch > 0 || self.no_fetch.map_or(false, |v| v),
+            gcc_upstream_branch: gcc_upstream_branch
+                .or(self.gcc_upstream_branch.take())
+                .or(Some("gnu/trunk".to_string()))
+                .unwrap(),
+            gccrs_dev_branch: gccrs_dev_branch
+                .or(self.gccrs_dev_branch.take())
+                .or(Some("upstream-gccrs/master".to_string()))
+                .unwrap(),
+            new_branch: new_branch
+                .or(self.to_branch.take())
+                .or(Some(create_new_branch_name("rebase")))
+                .unwrap(),
+            gccrs: gccrs.or(self.work.take().map(PathBuf::from)).unwrap(),
+            remote: remote.or(self.remote.take()),
+        })
+    }
+}
 
 #[derive(Clone, Subcommand)]
 enum SubCmd {
@@ -31,30 +157,27 @@ enum SubCmd {
         )]
         token_file: Option<String>,
 
-        #[arg(long, help = "GitHub project owner", default_value = "Rust-GCC")]
+        #[arg(long, help = "GitHub project owner")]
         github_project_owner: Option<String>,
 
-        #[arg(long, help = "GCC upstream branch", default_value = "gnu/trunk")]
-        gcc_upstream_branch: String,
+        #[arg(long, help = "GCC upstream branch")]
+        gcc_upstream_branch: Option<String>,
 
-        #[arg(long, help = "Do not update remotes")]
-        no_fetch: bool,
-
-        #[arg(
-            long,
-            help = "Only linearize commit sequence, do not rebase over updated upstream"
-        )]
-        linearize_only: bool,
+        #[arg(long, help = "Do not update remotes", action = clap::ArgAction::Count)]
+        no_fetch: u8,
 
         #[arg(
             long,
-            help = "gccrs development branch",
-            default_value = "gccrs/master"
+            help = "Only linearize commit sequence, do not rebase over updated upstream",
+            action = clap::ArgAction::Count
         )]
-        gccrs_dev_branch: String,
+        linearize_only: u8,
 
-        #[arg(long, help = "autosquash fixup commits")]
-        autosquash: bool,
+        #[arg(long, help = "gccrs development branch")]
+        gccrs_dev_branch: Option<String>,
+
+        #[arg(long, help = "autosquash fixup commits", action = clap::ArgAction::Count)]
+        autosquash: u8,
 
         #[arg(
             long,
@@ -70,7 +193,7 @@ enum SubCmd {
             long,
             help = "work directory which contains a copy of the gccrs respository"
         )]
-        work: PathBuf,
+        work: Option<PathBuf>,
     },
     /// Create a PR on `gccrs`'s repository containing the commits from master which haven't yet
     /// been formatted properly for upstreaming.
@@ -82,13 +205,12 @@ enum SubCmd {
         )]
         token_file: Option<String>,
 
-        #[arg(long, help = "GitHub project owner", default_value = "Rust-GCC")]
+        #[arg(long, help = "GitHub project owner")]
         github_project_owner: Option<String>,
 
         #[arg(
             long,
-            help = "Name for the base branch to be used for the pull-request. This should be an upstream branch.",
-            default_value = "gcc-patch-dev"
+            help = "Name for the base branch to be used for the pull-request. This should be an upstream branch."
         )]
         github_upstream_base: Option<String>,
 
@@ -98,28 +220,24 @@ enum SubCmd {
         )]
         to_branch: Option<String>,
 
-        #[arg(long, help = "Do not update remotes")]
-        no_fetch: bool,
+        #[arg(long, help = "Do not update remotes", action = clap::ArgAction::Count)]
+        no_fetch: u8,
 
-        #[arg(long, help = "Do not rebase onto latest upstream GCC")]
-        no_rebase: bool,
+        #[arg(long, help = "Do not rebase onto latest upstream GCC", action = clap::ArgAction::Count)]
+        no_rebase: u8,
 
-        #[arg(long, help = "GCC upstream branch", default_value = "gnu/trunk")]
-        gcc_upstream_branch: String,
+        #[arg(long, help = "GCC upstream branch")]
+        gcc_upstream_branch: Option<String>,
 
-        #[arg(
-            long,
-            help = "gccrs development branch.",
-            default_value = "gccrs/master"
-        )]
-        gccrs_dev_branch: String,
+        #[arg(long, help = "gccrs development branch.")]
+        gccrs_dev_branch: Option<String>,
 
         #[arg(
             short,
             long,
             help = "Work directory which contains a copy of the gccrs respository"
         )]
-        work: PathBuf,
+        work: Option<PathBuf>,
 
         #[arg(short, long, help = "Push the branch to the specified remote")]
         remote: Option<String>,
@@ -128,6 +246,9 @@ enum SubCmd {
 
 #[derive(Parser)]
 struct Args {
+    #[arg(long, help = "TOML config file")]
+    config: Option<String>,
+
     #[command(subcommand)]
     cmd: SubCmd,
 }
@@ -140,6 +261,13 @@ fn create_new_branch_name(topic: &str) -> String {
 async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     env_logger::init();
+
+    let mut conf = if let Some(toml_config) = args.config {
+        let content = std::fs::read_to_string(toml_config)?;
+        toml::from_str(&content)?
+    } else {
+        Config::new()
+    };
 
     match args.cmd {
         SubCmd::ChangeLogs => clog::check_clog_checker_output()?,
@@ -155,25 +283,20 @@ async fn main() -> anyhow::Result<()> {
             work,
             remote,
         } => {
-            let token = if let Some(tf) = token_file {
-                Some(fs::read_to_string(&tf)?.trim().to_string())
-            } else {
-                None
-            };
-
-            rebaseupstream::rebase_and_update(rebaseupstream::RebaseUpstreamOpt {
-                token,
+            let sconf = conf.to_rebaseupstreamopt(
+                token_file,
                 github_project_owner,
                 autosquash,
                 linearize_only,
                 no_fetch,
                 gcc_upstream_branch,
                 gccrs_dev_branch,
-                new_branch: new_branch.map_or(create_new_branch_name("rebase"), |s| s),
-                gccrs: work,
+                new_branch,
+                work,
                 remote,
-            })
-            .await?
+            )?;
+
+            rebaseupstream::rebase_and_update(sconf).await?
         }
 
         SubCmd::Upstream {
@@ -194,19 +317,20 @@ async fn main() -> anyhow::Result<()> {
                 None
             };
 
-            upstream::prepare_commits_bis(upstream::UpstreamOpt {
+            let sconf = conf.to_upstreamopt(
                 token,
                 github_project_owner,
                 github_upstream_base,
                 no_fetch,
                 no_rebase,
-                new_branch: new_branch.map_or(create_new_branch_name("upstream"), |s| s),
+                new_branch,
                 gcc_upstream_branch,
                 gccrs_dev_branch,
-                gccrs: work,
+                work,
                 remote,
-            })
-            .await?
+            )?;
+
+            upstream::prepare_commits_bis(sconf).await?
         }
     }
 
